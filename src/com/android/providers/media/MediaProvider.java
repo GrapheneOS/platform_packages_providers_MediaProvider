c448cd2daec623078294675f0db11b23c6fd88b5
Hyoungho Choi <hh46.choi1@samsung.corp-partner.google.com>
2024-11-08 13:56:31 +0000

    Fix getType with a redacted uri
    An app with shared redacted uri can't get its mime type properly when
    the app doesn't have access permission. The app can only access the
    redacted uri, not a converted uri
    
    Test: atest MediaProviderTests
    Bug: 368178777
    Flag: EXEMPT bugfix
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:c2b49fed8979862532eabdb9848350335ed437e0)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c5b31a4bb9175c289d6ee2d6c9c0f1cf6179af4a)
    Merged-In: I80cd5d0f89af43aaa776346551ed23725d3b1ff5
    Change-Id: I80cd5d0f89af43aaa776346551ed23725d3b1ff5

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -4304,8 +4304,9 @@ public class MediaProvider extends ContentProvider {
     @Override
     public String getType(Uri url) {
         if (isRedactedUri(url)) {
-            url = getUriForRedactedUri(url);
+            return queryForTypeAsCaller(url);
         }
+
         final int match = matchUri(url, true);
         switch (match) {
             case IMAGES_MEDIA_ID:
