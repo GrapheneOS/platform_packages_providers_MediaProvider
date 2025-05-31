2e39743fde883f29c321937fd76dc2a36c0cc3a5
maade93791 <70593890+maade69@users.noreply.github.com>
2025-06-01 01:07:35 +0300

    FuseDaemon: treat paths with ignorable code point as inaccessible
    Our existing generic hardening included alongside our Storage Scopes
    feature in 2022 (whether or not it's enabled) protected against many
    cases of this, but there were still cases relevant for "All files
    access" or the legacy equivalent for old target API levels.
    
    Co-authored-by: Fred Underwood <underwoodfred@proton.me>
    Co-authored-by: quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
    Co-authored-by: inthewaves <inthewaves@pm.me>

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -12638,4 +12638,17 @@ public class MediaProvider extends ContentProvider {
         return Flags.enableMaliciousAppDetector() && !isCallingPackageSystemGallery()
                 && !isCallingPackageSelf();
     }
+
+    @Keep
+    public boolean hasIgnorableCodePointsOnPathForFuse(@Nullable String path) {
+        if (path == null) {
+            return false;
+        }
+
+        if (FileUtils.hasIgnorableCodePointCharsOnPath(path)) {
+            return true;
+        }
+
+        return false;
+    }
 }
