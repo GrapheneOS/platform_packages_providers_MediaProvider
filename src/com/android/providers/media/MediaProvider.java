14962d8899063c3ac920ac843052c3bdb582b119
Himanshu Arora <hmarora@google.com>
2025-10-24 16:47:37 +0000

    Fix ACCESS_MEDIA_LOCATION bypass via SAF picker
    CVE-Info: CVE-2026-0024 | Severity: High
    
    When an app uses a picker to access media files, the picker should respect the app's permissions. Currently, it is possible to bypass the ACCESS_MEDIA_LOCATION permission and get unredacted location data.
    
    This change fixes this by having MediaProvider check the permissions of the app on whose behalf the media is being opened. When a `mediaCapabilitiesUid` is passed, MediaProvider now checks if that UID has the necessary permissions.
    
    Bug: 326211886
    Test: manual
    Flag: EXEMPT BUGFIX
    Change-Id: I7ad51535d5ae8a3803162f688c4794edbbcfb167

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -10822,7 +10822,7 @@ public class MediaProvider extends ContentProvider {
 
         // Figure out if we need to redact contents
         final boolean redactionNeeded = isRedactionNeededForOpenViaContentResolver(redactedUri,
-                ownerPackageName, file);
+                ownerPackageName, file, opts);
         long[] redactionRanges;
         try {
             redactionRanges = redactionNeeded ? RedactionUtils.getRedactionRanges(file)
@@ -10941,10 +10941,26 @@ public class MediaProvider extends ContentProvider {
     }
 
     private boolean isRedactionNeededForOpenViaContentResolver(Uri redactedUri,
-            String ownerPackageName, File file) {
+            String ownerPackageName, File file, Bundle opts) {
         // Redacted Uris should always redact information
         if (redactedUri != null) {
             return true;
+        }
+
+        // If the caller provides a media capabilities UID, we check if that UID has the
+        // PERMISSION_IS_REDACTION_NEEDED permission. If so, we redact the data. This is
+        // used for cases where an app is acting on behalf of another app, and we need
+        // to respect the capabilities of the app for which the action is being performed.
+        if (opts != null) {
+            final int mediaCapabilitiesUid = opts.getInt(MediaStore.EXTRA_MEDIA_CAPABILITIES_UID);
+            if (mediaCapabilitiesUid > 0) {
+                final LocalCallingIdentity identity = LocalCallingIdentity.fromExternal(
+                        getContext(),
+                        mUserCache, mediaCapabilitiesUid, null, null);
+                if (identity.hasPermission(PERMISSION_IS_REDACTION_NEEDED)) {
+                    return true;
+                }
+            }
         }
 
         final boolean callerIsOwner = Objects.equals(getCallingPackageOrSelf(), ownerPackageName);
