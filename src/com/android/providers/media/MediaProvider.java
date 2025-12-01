14625906e382eafa318c9e616a556b77d1466b92
Garvita Jain <garvitajain@google.com>
2025-12-01 11:02:37 +0000

    Throw exception on MediaStore createRequest for non-existent uri
    CVE-Info: CVE-2026-0035 | Severity: High
    
    Throw IllegalArgument exception if a caller requests
    CREATE_WRITE/DELETE/.._REQUEST for a uri that does not exist in the
    Files table at the time of request.
    
    BUG: 418773439
    Test: atest MediaProviderTests
    Flag: EXEMPT bugfix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:268fc3fb0a438abbc710687a9590cb80b3c0e8bc
    Merged-In: I00a3b15d8dbcd19afaec3980f7d7a07122bef640
    Change-Id: I00a3b15d8dbcd19afaec3980f7d7a07122bef640

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -8903,6 +8903,22 @@ public class MediaProvider extends ContentProvider {
             }
         }
 
+        // Do not allow to create request if the list contains a uri which does not exist
+        final LocalCallingIdentity token = clearLocalCallingIdentity();
+        try {
+            for (Uri uri : uris) {
+                try (Cursor c = queryForSingleItem(uri, new String[]{FileColumns._ID}, null, null,
+                        null)) {
+                    // queryForSingleItem method throws FileNotFoundException if no items were
+                    // found, or multiple items were found, or there was trouble reading the data.
+                } catch (FileNotFoundException e) {
+                    throw new IllegalArgumentException("Invalid Uri: " + uri, e);
+                }
+            }
+        } finally {
+            restoreLocalCallingIdentity(token);
+        }
+
         final Context context = getContext();
         final Intent intent = new Intent(method, null, context, PermissionActivity.class);
         extras.putInt(EXTRA_CALLING_PACKAGE_UID, getCallingUidOrSelf());
